8fb1ca41e6f4a6843271213bff253118475cf12e
Anton Tsarikovich <Anton Tsarikovich>
2017-03-03 20:48:43 +0300

    Factory Method

---
+++ Patterns/Patterns/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Patterns.Creational;
+
+namespace Patterns {
+    class Program {
+        static void Main(string[] args) {
+            Factory_Method fact = new Factory_Method();
+            fact.Run();
+        }
+    }
+}
